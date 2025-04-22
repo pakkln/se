68724e49d11ce9490b7a7a1840295a0f3aff0f7f
pakkln <pak.kokiln@gmail.com>
2025-04-22 16:09:10 +0700

    fix2

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const monthOfTheYear = (date = new Date()) => {
 
 try {
     document.getElementById('day').innerText = dayOfTheWeek();
+    document.getElementById('month').innerText = monthOfTheYear();
 } catch(err) {}
 
 
